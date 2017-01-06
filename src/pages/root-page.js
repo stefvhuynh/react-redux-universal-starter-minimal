079b0ac79bb4d470ddeaac19a11c03650a7d2188
Stefan Huynh <huynh.stefan@gmail.com>
2017-01-05 18:41:54 -0800

    Convert RootPage to class

--- src/pages/root-page.js
+++ src/pages/root-page.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { push } from "react-router-redux";
 
-const Root = ({ children, links, onLinkClick }) => {
-  const anchors = links.map((link) => {
-    return (
-      <a
-        key={`anchor-${link.label}-${link.path}`}
-        onClick={onLinkClick.bind(null, link.path)}
-      >
-        {link.label}
-      </a>
-    );
-  });
+class Root extends Component {
+  renderAnchors() {
+    const { links, onLinkClick } = this.props;
 
-  return (
-    <div>
-      <h1>Root Page</h1>
-      {anchors}
-      <br/>
-      {children}
-    </div>
-  );
-};
+    return links.map((link) => {
+      return (
+        <a
+          key={`anchor-${link.label}-${link.path}`}
+          onClick={onLinkClick.bind(null, link.path)}
+        >
+          {link.label}
+        </a>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Root Page</h1>
+        {this.renderAnchors()}
+        <br/>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 export default connect(
   () => {
